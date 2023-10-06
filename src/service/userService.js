0066ff501605a5bfdc1db8dcce99528975674276
unknown <nguyendinhcuong260302@gmail.com>
2023-10-06 15:22:18 +0700

    create user with ORM

--- src/service/userService.js
+++ src/service/userService.js
@@ -1,6 +1,7 @@
 import bcrypt from 'bcryptjs';
 import mysql from 'mysql2/promise';
 import bluebird from 'bluebird';
+import db from '../models/index';
 
 const salt = bcrypt.genSaltSync(10);
 
@@ -10,10 +11,12 @@ const hashPassword = (userPassword) => {
 
 const createNewUser = async (email, password, username) => {
     let hashPass = hashPassword(password);
-    const connection = await mysql.createConnection({ host: 'localhost', user: 'root', database: 'jwt', Promise: bluebird });
-
     try {
-        const [rows, fields] = await connection.execute('insert into user(email,password,username) values (?,?,?)', [email, hashPass, username]);
+        await db.User.create({
+            email: email,
+            password: hashPass,
+            username: username
+        });
     } catch (error) {
         console.log(error);
     }
